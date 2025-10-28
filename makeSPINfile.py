dd32ccf7d226dc8ab6ae3a99578e6d523f31d1c7
Rana <ranajelinek@gmail.com>
2025-10-28 14:03:27 -0400

    Update makeSPINfile.py

--- makeSPINfile.py
+++ makeSPINfile.py
@@ -1,5 +1,5 @@
 #Reads CHG file and outputs lines after second iteration of 90 90 90 into new file; SPIN.txt
-#input files: CHG
+#input files: CHGCAR
 #output files: SPIN
 import argparse
 
@@ -9,21 +9,33 @@ parser.add_argument("-chg", nargs='?', default = "./CHG", help="chg file locatio
 args = parser.parse_args()
 config = vars(args)
 count=0
+count2=0
+num2=0
 num=0
-
-print("To learn more about features, use makeSPINfile.py -h")
+molName='none'
 print("Running code to create SPIN.vasp file")
 #Reads CHG file and outputs lines after second iteration of 90 90 90 into new file; SPIN.txt
-always_print = True
-with open(config["chg"], 'r') as f, open("SPIN.vasp", "w") as y:
+always_print = False
+with open(config["chg"], 'r') as f:
+    molName=f.readline()
     for line in f:
         if '   90   90   90' in line:
+            count2 += 1
+with open(config["chg"], 'r') as f, open("SPIN.vasp", "w") as y:
+    for line in f:
+        if '   90   90   90' in line and num2<=(count2):
             always_print=False
             num += 1
-            if num == 2:
-                print(line, file=y, end='')
-                always_print= True
-                continue
+            num2 += 1
+        if molName in line and num==(count2-2):
+           always_print=True
+           if '   90   90   90' in line:
+               always_print=False
+        if num == count2:
+           print(line, file=y, end='')
+           always_print= True
+           continue
         if always_print:
             print(line, file=y, end='')
 print("Output files: SPIN.vasp")
+
