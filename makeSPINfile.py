050044b4dde8946167943c42b1eafe9dd9601c33
Rana <ranajelinek@gmail.com>
2025-11-05 14:30:23 -0500

    Update makeSPINfile.py

--- makeSPINfile.py
+++ makeSPINfile.py
@@ -1,5 +1,5 @@
-#Reads CHG file and outputs lines after second iteration of 90 90 90 into new file; SPIN.txt
-#input files: CHGCAR
+#Reads CHG file and outputs lines after second iteration of dimensions into new file; SPIN.txt
+#input files: CHG
 #output files: SPIN
 import argparse
 
@@ -13,23 +13,33 @@ count2=0
 num2=0
 num=0
 molName='none'
+dNums = ""
+#reads chg and finds dimension integers
+with open(config['chg'], 'r') as f:
+    for line in f:
+        parts = line.strip().split()
+        if len(parts) == 3:
+           if all('.' not in part and 'e' not in part.lower() for part in parts):
+              nums = list(map(int, parts))
+              dNums = line
+              break
 print("Running code to create SPIN.vasp file")
-#Reads CHG file and outputs lines after second iteration of 90 90 90 into new file; SPIN.txt
+#Reads CHG file and outputs lines after second iteration of the three integers into new file; SPIN.txt
 always_print = False
 with open(config["chg"], 'r') as f:
     molName=f.readline()
     for line in f:
-        if '   90   90   90' in line:
+        if dNums in line:
             count2 += 1
 with open(config["chg"], 'r') as f, open("SPIN.vasp", "w") as y:
     for line in f:
-        if '   90   90   90' in line and num2<=(count2):
+        if dNums in line and num2<=(count2):
             always_print=False
             num += 1
             num2 += 1
         if molName in line and num==(count2-2):
            always_print=True
-           if '   90   90   90' in line:
+           if dNums in line:
                always_print=False
         if num == count2:
            print(line, file=y, end='')
@@ -38,4 +48,3 @@ with open(config["chg"], 'r') as f, open("SPIN.vasp", "w") as y:
         if always_print:
             print(line, file=y, end='')
 print("Output files: SPIN.vasp")
-
